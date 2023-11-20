c4f75f7a15c2e8e973784ec17c11636e9dcff60d
Remi <remi.lhuillier@supdevinci-edu.fr>
2023-11-20 16:21:06 +0100

    fin actu

--- site/script.js
+++ site/script.js
@@ -1,12 +1,32 @@
-// Remplacez 'YOUR_VIDEO_ID' par l'ID de votre vidéo YouTube
-var videoID = 'O6L5wgTVtX8';
+// Remplacez 'YOUR_VIDEO_ID_1' et 'YOUR_VIDEO_ID_2' par les IDs de vos vidéos YouTube
+var videoID1 = 'O6L5wgTVtX8';
+var videoID2 = 'X2BOu4vmc20';
 
-var player;
+var player1, player2;
+
 function onYouTubeIframeAPIReady() {
-    player = new YT.Player('player', {
+    player1 = new YT.Player('player1', {
         height: '315',
         width: '560',
-        videoId: videoID,
+        videoId: videoID1,
+        playerVars: {
+            autoplay: 1,
+            controls: 1,
+            loop: 1,
+            rel: 0,
+            showinfo: 0,
+            modestbranding: 1,
+            fs: 1
+        },
+        events: {
+            'onReady': onPlayerReady,
+        }
+    });
+
+    player2 = new YT.Player('player2', {
+        height: '315',
+        width: '560',
+        videoId: videoID2,
         playerVars: {
             autoplay: 1,
             controls: 1,
